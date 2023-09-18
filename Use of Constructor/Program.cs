c7df86e125d3906d489c97f498b80dbc185b0b86
AnujRane-nimapinfotech <anuj@nimapinfotech.com>
2023-09-18 03:40:27 -0700

    Use Of Constructor

---
+++ Use of Constructor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Use_of_Constructor
+{
+    public class Vehicle
+    {
+        public Vehicle()
+        {
+            Console.WriteLine("Parameter less constructor called...");
+        }
+
+        public Vehicle(string name)
+        {
+            Console.WriteLine("Parameterized constructor called... " + name);
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle v1 = new Vehicle();
+            Vehicle v2 = new Vehicle("Hello World!");
+        }
+    }
+}
