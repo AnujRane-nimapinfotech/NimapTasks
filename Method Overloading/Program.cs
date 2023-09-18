10f5ba7068b793ba3cbfd3b697b45cc6f0e9b72e
AnujRane-nimapinfotech <anuj@nimapinfotech.com>
2023-09-18 03:28:03 -0700

    Method Overloading

---
+++ Method Overloading/Program.cs
@@ -0,0 +1,38 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overloading
+{
+    public class MethodOverloading
+    {
+        public void add()
+        {
+            Console.WriteLine("parameterless ADD function/method is called");
+        }
+
+        public void add(int a, int b)
+        {
+            int ans = a + b;
+            Console.WriteLine("a + b = " + ans);
+        }
+
+        public void add(int a, int b, int c)
+        {
+            int ans = a + b + c;
+            Console.WriteLine("a + b + c = " + ans);
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodOverloading obj = new MethodOverloading();
+            obj.add();
+            obj.add(1, 2);
+            obj.add(5, 10, 20);
+        }
+    }
+}
