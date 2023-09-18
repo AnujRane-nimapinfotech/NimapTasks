1b9fc87b9ea108cef6fc032ffaebf35e4ea7fe58
AnujRane-nimapinfotech <anuj@nimapinfotech.com>
2023-09-18 03:30:49 -0700

    Method Overriding

---
+++ Method Overriding/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overriding
+{
+    public class Animal
+    {
+        public virtual void sound()
+        {
+            Console.WriteLine("Animal Makes sound");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void sound()
+        {
+            Console.WriteLine("Dog Barks");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog d = new Dog();
+            d.sound();
+        }
+    }
+}
