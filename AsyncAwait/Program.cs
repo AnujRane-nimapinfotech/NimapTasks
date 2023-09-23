5bdf9a37ddd8399c197bb1d1ae8c90c5485864d4
AnujRane-nimapinfotech <anuj@nimapinfotech.com>
2023-09-23 15:42:55 +0530

    Async and Await

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Method();
+
+            Console.ReadKey();
+        }
+
+        public static async void Method()
+        {
+            await Task.Run(new Action(LongTask));
+
+            Console.WriteLine("New Thread");
+        }
+
+        public static void LongTask()
+        {
+            Thread.Sleep(20000);
+        }
+
+    }
+}
