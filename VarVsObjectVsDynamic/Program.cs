d7c09d1bf786f2ab721f9402aa864c2a68dc1467
AnujRane-nimapinfotech <anuj@nimapinfotech.com>
2023-09-23 12:53:22 +0530

    Var vs Object vs Dynamic

---
+++ VarVsObjectVsDynamic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarVsObjectVsDynamic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nameVar = "Anuj";
+            //nameVar = 5;      Will Show us error cause var was initialized as string
+
+            object nameObject = "Anuj Object";
+            string nameStringObject = (string) nameObject; //We have to cast to make it work
+
+            dynamic nameDynamic = "Anuj Dynamic";
+            string nameStringDynamic = nameDynamic;   //Here we can assign to other variable without casting
+        }
+    }
+}
