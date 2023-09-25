897b07c1f07e004756fee5e754a5627ee9f610d5
AnujRane-nimapinfotech <anuj@nimapinfotech.com>
2023-09-25 16:54:33 +0530

    Task Parallel Library

---
+++ TaskParallelLibrary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskParallelLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Thread thread = new Thread(RunMillionIterations);  // This will only utilize one processor
+            //thread.Start(); 
+            Parallel.For(0, 1000000, x => RunMillionIterations()); // This will utilize all processors
+        }
+
+        private static void RunMillionIterations()
+        {
+            string x = "";
+            for(int i = 0; i < 1000000; i++)
+            {
+                x = x + "s";
+            }
+        }
+    }
+}
